cdc897d0bfccf7eb788705947beefd305be7eb0f
Taruu <taruumax@gmail.com>
2024-08-28 15:24:08 +0300

    start next step develop

--- app/models.py
+++ app/models.py
@@ -2,37 +2,86 @@ from datetime import datetime
 from pathlib import Path
 from typing import Optional
 import enum
-from sqlmodel import Field, SQLModel, Enum, Column
+
+from sqlalchemy import create_engine
+from sqlmodel import Field, SQLModel, Enum, Column, Relationship
 
 
-class TagType(str, enum.Enum):
-    # https://ehwiki.org/wiki/Namespace
-    metadata = "metadata"
-    artist = "artist"
-    cosplayer = "cosplayer"
-    language = "language"
-    group = "group"
-    character = "character"
-    parody = "parody"
-    other = "other"
-    male = "male"
-    female = "female"
-    mixed = "mixed"
-    reclass = "reclass"
+# class TagType(str, enum.Enum):
+#     # https://ehwiki.org/wiki/Namespace
+#     temp = "temp"
+#     reclass = "reclass"
+#     metadata = "metadata"
+#     artist = "artist"
+#     cosplayer = "cosplayer"
+#     language = "language"
+#     group = "group"
+#     character = "character"
+#     parody = "parody"
+#     other = "other"
+#     male = "male"
+#     female = "female"
+#     mixed = "mixed"
+
+class TagsNovelLink(SQLModel, table=True):
+    tag_id: int | None = Field(default=None, foreign_key="tag.id", primary_key=True)
+    novel_id: int | None = Field(default=None, foreign_key="visualnovel.id", primary_key=True)
 
 
 class VisualNovel(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(unique=True)
+    gid: int | None = Field(default=None, index=True)
+    token: str | None = Field(default=None, index=True)
+
+    archive_path: Path
+    thumb_path: Path
+
     created_at: datetime
     added_at: datetime = Field(default_factory=lambda: datetime.now())
-    size: int = Field(default=0)
-    pages: int
-    path: Path = Field(unique=True)
-    cover_filename: str
+
+    title: str = Field(index=True)
+    title_jpn: str = Field(index=True)
+
+    file_count: int = Field(default=0)
+    file_size: int = Field(default=0)
+    
+    rating: float
+    tags: list["Tag"] = Relationship(back_populates="heroes", link_model=TagsNovelLink)
+    torrents: list["NovelTorrent"] = Relationship(back_populates="novel")
+
+
+class TagType(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+    name: str = Field(unique=True)
+    tags: list["Tag"] = Relationship(back_populates="tag_type")
 
 
 class Tag(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    type: Enum[TagType]
-    name: str
+    tag_type_id: int | None = Field(default=None, foreign_key="tagtype.id", primary_key=True)
+    tag_type: TagType | None = Relationship(back_populates="tags")
+
+    name: str = Field(unique=True, index=True)
+    novels: list[VisualNovel] = Relationship(back_populates="tags", link_model=TagsNovelLink)
+
+
+class NovelTorrent(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)  # Auto-incrementing primary key
+    novel_id: int | None = Field(default=None, foreign_key="visualnovel.id")
+
+    added: datetime
+
+    novel: VisualNovel = Relationship(back_populates="torrents")
+    hash: str = Field(index=True)
+    name: str = Field(index=True)
+    t_size: int
+    f_size: int
+
+
+if __name__ == "__main__":
+    sqlite_file_name = "database.db"
+    sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+    engine = create_engine(sqlite_url, echo=True)
+    SQLModel.metadata.create_all(engine)
--- app/routers/manga.py
+++ app/routers/manga.py
@@ -1,18 +1,33 @@
+# https://github.com/Ravencentric/archivefile
+import archivefile
 from fastapi import APIRouter
 
 router = APIRouter()
 
 
-@router.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
+@router.get('/get_novel_page/{gid}/{token}')
+async def get_archive_page(gid: int, token: str):
+    """Receive bytes page from novel """
+    pass
 
 
-@router.get("/users/me", tags=["users"])
-async def read_user_me():
-    return {"username": "fakecurrentuser"}
+@router.get('/novel_info/{gid}/{token}')
+async def get_novel_info(gid: int, token: str):
+    """Get info about novel (name, tags, torrents, path, size, etc)"""
 
 
-@router.get("/users/{username}", tags=["users"])
-async def read_user(username: str):
-    return {"username": username}
+@router.post('/novel_info/{gid}/{token}')
+async def update_novel_info(gid: int, token: str):
+    """Request form remote info about novel (name, tags, torrents, path, size, etc)"""
+
+
+@router.post('/scan_folder')
+async def scan_folder():
+    """Create task to scan folder from config file"""
+    pass
+
+
+@router.get('/scan_folder_status')
+async def scan_folder_status():
+    """Get current status scanning folder"""
+    pass

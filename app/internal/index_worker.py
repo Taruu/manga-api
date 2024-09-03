eed18ebc7d6afdf43b7d3ef6770dc87162cc067d
Taruu <taruumax@gmail.com>
2024-09-03 09:08:13 +0300

    init worker

--- app/internal/index_worker.py
+++ app/internal/index_worker.py
@@ -4,7 +4,26 @@ from ..config import novel_folder
 ARCHIVE_TYPES = ['zip', "7z", "tar.*", "tar", 'rar']
 
 
-def all_archives():
-    for archive_type in ARCHIVE_TYPES:
-        for archive_path in glob.glob(f"{novel_folder}/**/*.{archive_type}", recursive=True):
-            yield archive_path
+class IndexWorker:
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    @staticmethod
+    def all_archives():
+        for archive_type in ARCHIVE_TYPES:
+            for archive_path in glob.glob(f"{novel_folder}/**/*.{archive_type}", recursive=True):
+                yield archive_path
+
+    async def start(self):
+        pass
+
+    async def status(self):
+        pass
+
+    async def stop(self):
+        pass
+
